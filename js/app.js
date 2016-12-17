a4bf6c9ce5908458b99d24556c137edc2c7cc050
nms377 <nms377@nyu.edu>
2016-12-17 10:13:25 -1000

    Added homeworld for person4.

--- js/app.js
+++ js/app.js
@@ -5,14 +5,19 @@
 function reqListener(){
 	console.log('the response text', this.responseText);
 
-	//person
+	//person4
 	let person4 = JSON.parse(this.responseText);
 	console.log(person4);
 
-	//person name
+	//person4Name
 	let person4Name = person4.name;
 	console.log(person4Name);
 	document.getElementById('person4Name').innerHTML = person4Name;
+
+	//person4HomeWorld
+	let person4HomeWorld = person4.homeworld;
+	console.log(person4HomeWorld);
+	document.getElementById('person4HomeWorld').innerHTML = person4HomeWorld;
 
 		//will load person4's name when reqListener loads person's name
 		function reqListenerName(){
@@ -20,8 +25,22 @@ function reqListener(){
 			let name = personsName.name;
 			document.getElementById('person4Name').innerHTML = name;
 		}
+
+		//will load person4's homeworld when reqListener loads person4 and person4's name
+		function person4HomeWorldIs(){
+			let homeworldReq = JSON.parse(this.responseText);
+			let homeworldIs = homeworldReq.homeworld;
+			document.getElementById('person4HomeWorld').innerHTML = homeworldIs;
+		}
+
+		//will run homeworld function when window loads person4 and person4Name
+		var homeWorld = new XMLHttpRequest();
+		homeWorld.addEventListener('load', person4HomeWorldIs);
+		homeWorld.open('GET', 'http://swapi.co/api/people/4/');
+		homeWorld.send();
 }
 
+	//will run reqListener function returning person4 and person4Name
 	const oReq = new XMLHttpRequest();
 	oReq.addEventListener('load', reqListener);
 	oReq.open('GET', 'http://swapi.co/api/people/4/');
